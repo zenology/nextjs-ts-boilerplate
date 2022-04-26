4bcd39e170bfa73f675c7c66746eae60442a7124
Noppanan Sinroungrong <noppanans@arv.co.th>
2022-04-26 19:46:02 +0700

    format code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
+	return <Component {...pageProps} />
 }
 
 export default MyApp
